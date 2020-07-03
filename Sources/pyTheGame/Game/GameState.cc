6bd55f44073ea8da7589fcd7f6f8fbda4c081141
Junyeong Park <jyp10987@gmail.com>
2020-07-03 13:27:40 +0900

    feat(pyapi): Create GameState class

---
+++ Sources/pyTheGame/Game/GameState.cc
@@ -0,0 +1,28 @@
+#include <pyTheGame/Game/GameState.hpp>
+
+#include <TheGame/Game/GameState.hpp>
+
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+using namespace TheGame;
+
+void buildGameResult(py::module& m)
+{
+    py::enum_<GameResult>(m, "GameResult")
+        .value("NOT_FINISHED", GameResult::NOT_FINISHED)
+        .value("WIN", GameResult::WIN)
+        .value("LOSE", GameResult::LOSE)
+        .export_values();
+}
+
+void buildGameState(py::module& m)
+{
+    py::class_<GameState>(m, "GameState")
+        .def_readonly_static("STACK_COUNT", &GameState::STACK_COUNT)
+        .def_readwrite("cards_to_have", &GameState::CardsToHave)
+        .def_readwrite("min_cards_to_have", &GameState::MinCardsToDraw)
+        .def_readwrite("result", &GameState::result)
+        .def_readwrite("cards", &GameState::Cards)
+        .def_readwrite("card_stacks", &GameState::CardStacks);
+}
--- Sources/pyTheGame/main.cc
+++ Sources/pyTheGame/main.cc
@@ -3,10 +3,15 @@
 #include <pyTheGame/Cards/Card.hpp>
 #include <pyTheGame/Cards/CardStack.hpp>
 
+#include <pyTheGame/Game/GameState.hpp>
+
 PYBIND11_MODULE(pyTheGame, m)
 {
     m.doc() = R"pbdoc(TheGame simulator)pbdoc";
 
     buildCard(m);
     buildCardStack(m);
+
+    buildGameResult(m);
+    buildGameState(m);
 }
