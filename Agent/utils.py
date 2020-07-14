e595ee1572b5e5d6df8338160e084351c5abb924
Junyeong Park <jyp10987@gmail.com>
2020-07-14 22:30:54 +0900

    feat(agent): Implement Action Mask

--- Agent/utils.py
+++ Agent/utils.py
@@ -1,14 +1,14 @@
 import pyTheGame
 import numpy as np
 
-def print_game_state(state):
+def print_game_state(state: pyTheGame.GameState):
     print('card stacks: ', end='')
 
     for stack in state.card_stacks:
         print(int(stack.top_card()), end=' ')
     print()
 
-def print_hand(player):
+def print_hand(player: pyTheGame.Player):
     print('your hands: ', end='')
 
     for card in player.cards:
@@ -18,19 +18,19 @@ def print_hand(player):
 # we need to present numbers from 1 to 100
 # so, we need ceil(log_2(100))=7 bits for one card
 BITS_FOR_NUM = 7
-def bin_array(card):
+def bin_array(card: pyTheGame.Card):
     return np.array(list(np.binary_repr(int(card)).zfill(BITS_FOR_NUM))).astype(np.int8)
 
 
-def card_stacks_to_tensor(state):
-    stack_0 = bin_array(state.card_stacks[0].top_card())
-    stack_1 = bin_array(state.card_stacks[1].top_card())
-    stack_2 = bin_array(state.card_stacks[2].top_card())
-    stack_3 = bin_array(state.card_stacks[3].top_card())
+def card_stacks_to_tensor(state: pyTheGame.GameState):
+    stack = []
 
-    return np.concatenate((stack_0, stack_1, stack_2, stack_3))
+    for i in range(state.STACK_COUNT):
+        stack.append(bin_array(state.card_stacks[i].top_card()))
 
-def hands_to_tensor(player):
+    return np.concatenate(stack)
+
+def hands_to_tensor(player: pyTheGame.Game):
     max_of_hands = player.game.get_state().cards_to_have
     num_of_hands = len(player.cards)
 
@@ -45,11 +45,23 @@ def hands_to_tensor(player):
 
     return np.concatenate((num_vec, hand_vec))
 
-def state_to_tensor(player):
+def state_to_tensor(player: pyTheGame.Player):
     card_stacks_tensor = card_stacks_to_tensor(player.game.get_state())
     hands_tensor = hands_to_tensor(player)
 
     return np.concatenate((card_stacks_tensor, hands_tensor))
+
+def action_mask(player: pyTheGame.Player):
+    state = player.game.get_state()
+
+    mask = np.zeros(state.cards_to_have * state.STACK_COUNT)
+
+    for i, card in enumerate(player.cards):
+        for j in range(state.STACK_COUNT):
+            if state.card_stacks[j].stackable(card):
+                mask[i * state.STACK_COUNT + j] = 1
+
+    return mask
 
 if __name__ == '__main__':
     game = pyTheGame.Game()
@@ -65,4 +77,18 @@ if __name__ == '__main__':
     print(card_stacks_to_tensor(game.get_state()))
     print(hands_to_tensor(player))
     print(state_to_tensor(player))
+    print(action_mask(player))
+
+    game.process_turn(game, pyTheGame.StackTask(player.cards[0], 0))
+    game.process_turn(game, pyTheGame.StackTask(player.cards[0], 1))
+    game.end_turn()
+
+    print()
+    print_game_state(game.get_state())
+    print_hand(player)
+
+    print(card_stacks_to_tensor(game.get_state()))
+    print(hands_to_tensor(player))
+    print(state_to_tensor(player))
+    print(action_mask(player))
 
